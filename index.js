dd3c6d413ef80a0b38c569acd0b3184d9ec9f5b8
vladimir.s <vladimir.s@ezscratch.com>
2013-09-13 12:00:58 +0400

    uncomment, move require

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ var http = require('http'),
     b = browserify();
 b.add('./lib/client.js').bundle({}, function (err, src) {
 	'use strict';
-    //src = UglifyJS.minify(src.toString(), {fromString: true});
+    src = UglifyJS.minify(src.toString(), {fromString: true});
     var server = http.createServer(function (req, res) {
 		res.writeHead(200);
-        res.end('<script>' + src/*.code*/ + '</script>');
+        res.end('<script>' + src.code + '</script>');
 	}),
-       port = Math.floor(Math.random() * 8080);
+       port = process.env.VCAP_SERVICES || Math.floor(Math.random() * 8080);
     console.log('http://localhost:' + port + '/');
 	server.listen(port);
 });
