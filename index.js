02f6afa1baaeae12ae1c8e9798f00664fa4563be
vladimir.s <vladimir.s@ezscratch.com>
2013-09-13 11:44:24 +0400

    part1, part 2 request

---
+++ index.js
@@ -0,0 +1,16 @@
+/*global require*/
+var http = require('http'),
+    UglifyJS = require('uglify-js'),
+    browserify = require('browserify'),
+    b = browserify();
+b.add('./lib/client.js').bundle({}, function (err, src) {
+	'use strict';
+    //src = UglifyJS.minify(src.toString(), {fromString: true});
+    var server = http.createServer(function (req, res) {
+		res.writeHead(200);
+        res.end('<script>' + src/*.code*/ + '</script>');
+	}),
+       port = Math.floor(Math.random() * 8080);
+    console.log('http://localhost:' + port + '/');
+	server.listen(port);
+});
